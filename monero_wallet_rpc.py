02bbd264d7b91f1c6c7e215e8fe256fc68316047
Nick <shoupnb@gmail.com>
2018-05-26 09:39:02 -0600

    modified the post method to rpc, cleaned up some code in monerojdaemon.py

--- monero_wallet_rpc.py
+++ monero_wallet_rpc.py
@@ -3,11 +3,10 @@ import json
 from requests.auth import HTTPDigestAuth
 import os
 import binascii
-#monero-wallet-rpc.exe --wallet-file test --daemon-address 127.0.0.1:28081 --rpc-bind-port 18083 --daemon-login test:test --password test --rpc-login username:password
-#http://104.236.186.62:28081/ testnet daemon
+
 
 class MoneroWalletRpc:
-    def __init__(self, rpc_url=str, user=str, pwd=str):
+    def __init__(self, rpc_url=str, user=None, pwd=None):
         self.rpc_url = rpc_url
         self.user = user
         self.password = pwd
@@ -32,21 +31,37 @@ class MoneroWalletRpc:
         return response
 
     def get_payments(self, payment_id):
-        response = self.simple_method("get_payments", payment_id)
-        return response
+        data = '{"jsonrpc":"2.0","id":"0","method":"get_payments","params":{"payment_id":"'+payment_id+'"}}'
+        response = requests.post(self.rpc_url, headers=self.headers,
+                                 data=data,
+                                 auth=HTTPDigestAuth(self.user, self.password))
+        return json.dumps(response.json(), indent=5)
 
     def export_key_images(self):
         response = self.simple_method("export_key_images")
-        return  response
+        return response
 
     def rescan_spent(self):
         response = self.simple_method("rescan_spent")
         return response
 
+    def start_mining(self,thread_count:int, background_mining:bool, ignore_battery:bool):
+        #TODO replace params
+        data = '{"jsonrpc":"2.0","id":"0",' \
+               '"method":"start_mining",' \
+               '"params":{"threads_count":1,' \
+               '"do_background_mining":true,' \
+               '"ignore_battery":true}}'
+        response = requests.post('http://localhost:18082/json_rpc', headers=self.headers, data=data)
+        return json.dumps(response.json(), indent=5)
 
     def stop_mining(self):
         response = self.simple_method("stop_mining")
         return response
+
+    def stop_wallet(self):
+        response = self.simple_method("stop_wallet")
+        return json.dumps(response.json(), indent=5)
 
     def get_languages(self):
         response = self.simple_method("get_languages")
@@ -70,6 +85,23 @@ class MoneroWalletRpc:
         response = self.simple_method("open_wallet", rpc_input)
         return response
 
+    def post_to_monerod_rpc(self, method: str, *params):
+        # define standard json header
+        if params is not None:
+            rpc_input = {"method": method, "params": params}
+        else:
+            rpc_input = {"method": method}
+        # add standard rpc values
+        rpc_input.update({"jsonrpc": "2.0", "id": "0"})
+        # execute the rpc request
+        response = requests.post(
+            self.rpc_url,
+            data=json.dumps(rpc_input),
+            headers=self.headers,
+            auth=HTTPDigestAuth(self.user, self.password)
+        )
+        return json.dumps(response.json(), indent=5)
+
     def simple_method(self, method_name, params=None):
         url = self.rpc_url
         headers = self.headers
@@ -89,7 +121,7 @@ class MoneroWalletRpc:
             data=json.dumps(self.rpc_input),
             headers=headers,
             auth=HTTPDigestAuth(self.user, self.password))
-        return response.json()
+        return json.dumps(response.json(), indent=5)
 
     def transfer(self, transactions, mixin=4,):
         url = self.rpc_url
@@ -98,13 +130,13 @@ class MoneroWalletRpc:
 
         recipients = []
         for address, amount in transactions.items():
-            int_amount = int(get_amount(amount))
-            assert amount == float(get_money(str(int_amount))), "Amount conversion failed"
+            int_amount = int(self.get_amount(amount))
+            assert amount == float(self.get_money(str(int_amount))), "Amount conversion failed"
             recipients.append({"address": address, "amount": int_amount})
         print(recipients)
 
         # get some random payment_id
-        payment_id = get_payment_id()
+        payment_id = self.get_payment_id()
 
         # simplewallet' procedure/method to call
         rpc_input = {
@@ -129,47 +161,62 @@ class MoneroWalletRpc:
         print(json.dumps(response.json(), indent=4))
 
 
-def get_amount(amount):
-    """encode amount (float number) to the cryptonote format. Hope its correct.
-    Based on C++ code:
-    https://github.com/monero-project/bitmonero/blob/master/src/cryptonote_core/cryptonote_format_utils.cpp#L211
-    """
-    cryptonote_display_decimal_point = 12
-    str_amount = str(amount)
-    fraction_size = 0
-    if '.' in str_amount:
-        point_index = str_amount.index('.')
-        fraction_size = len(str_amount) - point_index - 1
-        while fraction_size < cryptonote_display_decimal_point and '0' == str_amount[-1]:
-            print(44)
-            str_amount = str_amount[:-1]
-            fraction_size = fraction_size - 1
-        if cryptonote_display_decimal_point < fraction_size:
+    def get_amount(self, amount):
+        """encode amount (float number) to the cryptonote format. Hope its correct.
+        Based on C++ code:
+        https://github.com/monero-project/bitmonero/blob/master/src/cryptonote_core/cryptonote_format_utils.cpp#L211
+        """
+        cryptonote_display_decimal_point = 12
+        str_amount = str(amount)
+        fraction_size = 0
+        if '.' in str_amount:
+            point_index = str_amount.index('.')
+            fraction_size = len(str_amount) - point_index - 1
+            while fraction_size < cryptonote_display_decimal_point and '0' == str_amount[-1]:
+                print(44)
+                str_amount = str_amount[:-1]
+                fraction_size = fraction_size - 1
+            if cryptonote_display_decimal_point < fraction_size:
+                return False
+            str_amount = str_amount[:point_index] + str_amount[point_index+1:]
+        if not str_amount:
             return False
-        str_amount = str_amount[:point_index] + str_amount[point_index+1:]
-    if not str_amount:
-        return False
-    if fraction_size < cryptonote_display_decimal_point:
-        str_amount = str_amount + '0'*(cryptonote_display_decimal_point - fraction_size)
-    return str_amount
+        if fraction_size < cryptonote_display_decimal_point:
+            str_amount = str_amount + '0'*(cryptonote_display_decimal_point - fraction_size)
+        return str_amount
 
 
-def get_money(amount):
-    """decode cryptonote amount format to user friendly format. Hope its correct.
-    Based on C++ code:
-    https://github.com/monero-project/bitmonero/blob/master/src/cryptonote_core/cryptonote_format_utils.cpp#L751
-    """
-    cryptonote_display_decimal_point = 12
-    s = amount
-    if len(s) < cryptonote_display_decimal_point + 1:
-        # add some trailing zeros, if needed, to have constant width
-        s = '0' * (cryptonote_display_decimal_point + 1 - len(s)) + s
-    idx = len(s) - cryptonote_display_decimal_point
-    s = s[0:idx] + "." + s[idx:]
-    return s
+    def get_money(self, amount):
+        """decode cryptonote amount format to user friendly format. Hope its correct.
+        Based on C++ code:
+        https://github.com/monero-project/bitmonero/blob/master/src/cryptonote_core/cryptonote_format_utils.cpp#L751
+        """
+        cryptonote_display_decimal_point = 12
+        s = amount
+        if len(s) < cryptonote_display_decimal_point + 1:
+            # add some trailing zeros, if needed, to have constant width
+            s = '0' * (cryptonote_display_decimal_point + 1 - len(s)) + s
+        idx = len(s) - cryptonote_display_decimal_point
+        s = s[0:idx] + "." + s[idx:]
+        return s
 
 
-def get_payment_id():
-    random_32_bytes = os.urandom(32)
-    payment_id = "".join(map(chr, binascii.hexlify(random_32_bytes)))
-    return payment_id
+    def get_payment_id(self):
+        random_32_bytes = os.urandom(32)
+        payment_id = "".join(map(chr, binascii.hexlify(random_32_bytes)))
+        return payment_id
+
+
+    def make_uri_payment(self, address:str, amount:int,
+                         payment_id:str, tx_description:str, recipient_name:str ):
+        data = '{"jsonrpc":"2.0","id":"0","method":"make_uri",' \
+               '"params":{"address":"44AFFq5kSiGBoZ4NMDwYtN18obc8AemS33DBLWs3H7otXft3XjrpDtQGv7SqSsaBYBb98uNbr2VBBEt7f2wfn3RVGQBEP3A",' \
+               '"amount":10,"payment_id":"0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",' \
+               '"tx_description":"Testing out the make_uri function.",' \
+               '"recipient_name":"Monero Project donation address"}}'
+        response = requests.post(
+            self.rpc_url,
+            data=data,
+            headers=self.headers,
+            auth=HTTPDigestAuth(self.user, self.password))
+        return json.dumps(response.json(), indent=5)
--- monerojdaemon.py
+++ monerojdaemon.py
@@ -2,26 +2,24 @@ import requests
 import json
 from requests.auth import HTTPDigestAuth
 
-class MoneroDaemonRpc:
 
-    def __init__(self, rpc_url: str, *user: str):
+class MoneroDaemonRpc:
+    def __init__(self, rpc_url: str, user: str=None, password: str=None):
         self.rpc_url = rpc_url
         self.headers = {'Content-Type': 'application/json'}
-        self.user = user[0]
-        self.password = [1]
+        self.user = user
+        self.password = password
 
-    def post_to_monerod_rpc(self, method:str, *params):
+    def post_to_monerod_rpc(self, method:str, *params: dict):
         # define standard json header
-        if params is not None:
-            rpc_input = {"method": method, "params": params}
+        if len(params) > 0 and params is not None:
+            rpc_input = json.dumps({"jsonrpc": "2.0", "id": "0","method": method, "params": params[0]})
         else:
-            rpc_input = {"method": method}
-        # add standard rpc values
-        rpc_input.update({"jsonrpc": "2.0", "id": "0"})
-        # execute the rpc request
+            rpc_input = json.dumps({"jsonrpc": "2.0", "id": "0","method": method})
+        print(rpc_input)
         response = requests.post(
             self.rpc_url,
-            data=json.dumps(rpc_input),
+            data=rpc_input,
             headers=self.headers,
             auth=HTTPDigestAuth(self.user, self.password)
         )
@@ -30,31 +28,31 @@ class MoneroDaemonRpc:
     def get_block_count(self):
         return self.post_to_monerod_rpc("getblockcount")
 
-    def get_block(self, height=int):
-        data = '{"jsonrpc":"2.0","id":"0","method":"getblock","params":{"height":'+str(height)+'}}'
-        response = requests.post(self.rpc_url, headers=self.headers, data=data)
-        return json.dumps(response.json())
+    def get_block(self, block):
+        if type(block) is str:
+            params = {"height": str(block)}
+        if type(block) is int:
+            params = {"height": block}
+        return self.post_to_monerod_rpc("getblock", params)
 
-    def on_getblockhash(self, block=int):
+    def on_getblockhash(self, block: int):
         return self.post_to_monerod_rpc("on_getblockhash", block)
 
-    def get_block_template(self, address=str, reserve_size=int):
-        data = '{"jsonrpc":"2.0","id":"0","method":"getblocktemplate","params":{"wallet_address":"'+address+'","reserve_size":'+str(reserve_size)+'}'
-        response = requests.post('http://127.0.0.1:18081/json_rpc', headers=self.headers, data=data)
-        return json.dumps(response.json())
+    def get_block_template(self, address: str, reserve_size: int):
+
+        params = {"wallet_address": address, "reserve_size": reserve_size}
+        return self.post_to_monerod_rpc("getblocktemplate", params)
 
     def get_last_block_header(self):
         return self.post_to_monerod_rpc("getlastblockheader")
 
-    def get_block_header_by_height(self, block_num=int):
-        params = {"params": {"height": block_num}}
+    def get_block_header_by_height(self, block_num: int):
+        params = {"height": block_num}
         return self.post_to_monerod_rpc("getblockheaderbyheight", params)
 
-    def get_block_header_by_hash(self, hash=str):
-        #TODO clean this up and find a better way to implement with other method of rpc_post method
-        data = '{"jsonrpc":"2.0","id":"0","method":"getblockheaderbyhash","params":{"hash":"'+hash+'"}}'
-        response = requests.post(self.rpc_url, headers=self.headers, data=data)
-        return json.dumps(response.json(), indent=5)
+    def get_block_header_by_hash(self, hash: str):
+        params = {"hash": hash}
+        return self.post_to_monerod_rpc("getblockheaderbyhash", params)
 
     def get_connections(self):
         return self.post_to_monerod_rpc("get_connections")
@@ -68,22 +66,27 @@ class MoneroDaemonRpc:
     def get_fee_estimate(self):
         return self.post_to_monerod_rpc("get_fee_estimate")
 
-    def submit_block(self, block_blob=str):
-        data = r'{"jsonrpc": "2.0","id": "0", "method": "submitblock","params": '+ block_blob +'}'
+    def submit_block(self, block_blob: str):
+        data = r'{"jsonrpc": "2.0","id": "0", "method": "submitblock","params": ' + block_blob + '}'
         response = requests.post(self.rpc_url, headers=self.headers, data=data)
         return json.dumps(response.json())
 
-    def set_bans(self):
-        #TODO add list to input parameters and create ban object
+#TODO look into this more
+    def set_bans(self, list_ips, time):
+        #params = {"bans":[{"ip":838969536,"ban":true,"seconds":30}]}}
+        #TODO add list to input parameters and create ban object/class
         data = r'{"jsonrpc":"2.0","id":"0","method":"set_bans","params":{"bans":[{"ip":838969536,"ban":true,"seconds":30}]}}'
         response = requests.post(self.rpc_url, headers=self.headers, data=data)
         print(response.headers)
         return json.dumps(response.json(), indent=5)
-        url = self.rpc_url.replace('json_rpc', 'stop_daemon')
-        response = requests.post(url, headers=self.headers)
 
     def get_bans(self):
         return self.post_to_monerod_rpc("get_bans")
+
+    def stop_daemon(self):
+        url = self.rpc_url.replace('json_rpc', 'stop_daemon')
+        response = requests.post(url, headers=self.headers)
+        return json.dumps(response.json(), indent=5)
 
     #get_transaction_pool
     def get_transaction_pool(self):
@@ -94,13 +97,17 @@ class MoneroDaemonRpc:
 
     def get_transactions(self, tx_hash=str):
         data = r'{"txs_hashes":["'+tx_hash+'"]}'
-        #TODO replace rpc_url below
         url = self.rpc_url.replace('json_rpc', 'gettransactions')
         response = requests.post(url, headers=self.headers, data=data)
         return response.text
 
-    def is_key_image_spent(self, key_images=list):
-        data = '{"key_images":'+str(key_images)+' }'
+    def is_key_image_spent(self, key_images: list):
+        keys = ""
+        for key in key_images[:-1]:
+            keys += '"' + key + '",'
+        else:
+            keys += '"' + key + '"'
+        data = '{"key_images":['+keys+']}'
         url = self.rpc_url.replace('json_rpc', 'is_key_image_spent')
         response = requests.post(url, headers=self.headers, data=data)
         return response.text
@@ -108,9 +115,11 @@ class MoneroDaemonRpc:
     def send_draw_transaction(self, tx_hash=str, do_not_relay=bool):
         data = ''
         if do_not_relay is True:
-            data = '{"tx_as_hex":' + tx_hash + ', "do_not_relay":true}'
+            data = '{"tx_as_hex":'+tx_hash+', "do_not_relay":true}'
         else:
-            data = '{"tx_as_hex":' + tx_hash + ', "do_not_relay":false}'
+            data = '{"tx_as_hex":'+tx_hash+', "do_not_relay":false}'
         url = self.rpc_url.replace('json_rpc', 'sendrawtransaction')
         response = requests.post(url, headers=self.headers, data=data)
         return response.text
+
+
