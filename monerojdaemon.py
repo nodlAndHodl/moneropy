49ad7840e65b9d20b55cd2ebebee9ac2d7711039
Nick <shoupnb@gmail.com>
2018-05-28 20:26:51 -0600

    cleaned up a couple things added some additonal text handling for response for get_transaction_ppol

--- monerojdaemon.py
+++ monerojdaemon.py
@@ -10,13 +10,14 @@ class MoneroDaemonRpc:
         self.user = user
         self.password = password
 
-    def post_to_monerod_rpc(self, method:str, *params: dict):
+    def post_to_monerod_rpc(self, method: str, params=None):
         # define standard json header
-        if len(params) > 0 and params is not None:
-            rpc_input = json.dumps({"jsonrpc": "2.0", "id": "0","method": method, "params": params[0]})
+        if params is not None:
+            rpc_input = json.dumps({"jsonrpc": "2.0", "id": "0",
+                                    "method": method, "params": params})
         else:
-            rpc_input = json.dumps({"jsonrpc": "2.0", "id": "0","method": method})
-        print(rpc_input)
+            rpc_input = json.dumps({"jsonrpc": "2.0", "id": "0",
+                                    "method": method})
         response = requests.post(
             self.rpc_url,
             data=rpc_input,
@@ -24,6 +25,19 @@ class MoneroDaemonRpc:
             auth=HTTPDigestAuth(self.user, self.password)
         )
         return json.dumps(response.json(), indent=5)
+
+    def json_rpc_modify_post(self, replacement_uri: str, to_text:bool, data=None):
+        url = self.rpc_url.replace('json_rpc', replacement_uri)
+        response = requests.post(
+            url,
+            headers=self.headers,
+            data=data,
+            auth=HTTPDigestAuth(self.user, self.password)
+        )
+        if to_text is False:
+            return json.dumps(response.json(), indent=5)
+        else:
+            return response.text
 
     def get_block_count(self):
         return self.post_to_monerod_rpc("getblockcount")
@@ -33,13 +47,13 @@ class MoneroDaemonRpc:
             params = {"height": str(block)}
         if type(block) is int:
             params = {"height": block}
+            print(type(params))
         return self.post_to_monerod_rpc("getblock", params)
 
     def on_getblockhash(self, block: int):
-        return self.post_to_monerod_rpc("on_getblockhash", block)
+        return self.post_to_monerod_rpc("on_getblockhash", [block])
 
     def get_block_template(self, address: str, reserve_size: int):
-
         params = {"wallet_address": address, "reserve_size": reserve_size}
         return self.post_to_monerod_rpc("getblocktemplate", params)
 
@@ -84,16 +98,10 @@ class MoneroDaemonRpc:
         return self.post_to_monerod_rpc("get_bans")
 
     def stop_daemon(self):
-        url = self.rpc_url.replace('json_rpc', 'stop_daemon')
-        response = requests.post(url, headers=self.headers)
-        return json.dumps(response.json(), indent=5)
+        return self.json_rpc_modify_post("stop_daemon", False)
 
-    #get_transaction_pool
     def get_transaction_pool(self):
-        response = requests.get(self.rpc_url, headers=self.headers)
-        print(response.headers)
-        #This returns a text response(can parse into json with some additional work)
-        return response.text
+        return self.json_rpc_modify_post("get_transaction_pool", True)
 
     def get_transactions(self, tx_hash=str):
         data = r'{"txs_hashes":["'+tx_hash+'"]}'
